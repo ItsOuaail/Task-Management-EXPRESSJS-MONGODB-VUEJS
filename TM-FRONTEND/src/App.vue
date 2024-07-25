15c86e36e85ec3a5d9dd049c1917299d2531d4ca
OuaailElaouad <2003waelelaouad@gmail.com>
2024-07-25 23:55:58 +0100

    Dependencied for FRontend

---
+++ TM-FRONTEND/src/App.vue
@@ -0,0 +1,7 @@
+<template>
+  <h1 class="text-3xl">Hello world</h1>
+  <button class="btn btn-info">Info</button>
+<button class="btn btn-success">Success</button>
+<button class="btn btn-warning">Warning</button>
+<button class="btn btn-error">Error</button>
+</template>
